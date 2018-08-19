e2783ea4ecb2c7f423fbc44c2bc7ce7d836df3dc
Raphael Stoeckli <github@rabanti.ch>
2018-08-19 21:44:11 +0200

    Update v1.2.3
    Update v1.2.3

--- src/main/java/ch/rabanti/nanoxlsx4j/Version.java
+++ src/main/java/ch/rabanti/nanoxlsx4j/Version.java
@@ -20,6 +20,6 @@ public final class Version {
     /**
      * Version of the library
      */
-    public static final String VERSION = "1.2200"; // 1.2.2
+    public static final String VERSION = "1.2300"; // 1.2.3
     
 }
