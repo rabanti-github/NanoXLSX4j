037700d162cb76f5b63850e4bc980f95d69ebd8d
Raphael Stoeckli <github@rabanti.ch>
2021-08-30 22:58:18 +0200

    Added exception unit tests

---
+++ src/test/java/ch/rabanti/nanoxlsx4j/misc/ExceptionTest.java
@@ -0,0 +1,93 @@
+package ch.rabanti.nanoxlsx4j.misc;
+
+import ch.rabanti.nanoxlsx4j.exceptions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExceptionTest {
+    // For code coverage
+    @DisplayName("Test of the FormatException (summary)")
+    @Test()
+    void formatExceptionTest()
+    {
+        FormatException exception = new FormatException();
+        assertNotEquals("", exception.getMessage()); // Gets a generated message my the base class
+        assertNull(exception.getInnerException());
+
+        exception = new FormatException("test");
+        assertEquals("test", exception.getMessage());
+        assertNull(exception.getInnerException());
+
+        IllegalArgumentException  inner = new IllegalArgumentException ("inner message");
+        exception = new FormatException("test", inner);
+        assertEquals("test", exception.getMessage());
+        assertNotNull(exception.getInnerException());
+        assertEquals(IllegalArgumentException.class, exception.getInnerException().getClass());
+        assertEquals("inner message", exception.getInnerException().getMessage());
+    }
+
+
+    @DisplayName("Test of the  IOExceptio (summary)")
+    @Test()
+    void iOExceptionTest()
+    {
+        IOException exception = new IOException();
+        assertNotEquals("", exception.getMessage()); // Gets a generated message my the base class
+        assertNull(exception.getInnerException());
+
+        exception = new IOException("test");
+        assertEquals("test", exception.getMessage());
+        assertNull(exception.getInnerException());
+
+        IllegalArgumentException  inner = new IllegalArgumentException ("inner message");
+        exception = new IOException("test", inner);
+        assertEquals("test", exception.getMessage());
+        assertNotNull(exception.getInnerException());
+        assertEquals(IllegalArgumentException.class, exception.getInnerException().getClass());
+        assertEquals("inner message", exception.getInnerException().getMessage());
+    }
+
+
+    @DisplayName("Test of the RangeException (summary)")
+    @Test()
+    void rangeExceptionTest()
+    {
+        RangeException exception = new RangeException();
+        assertNotEquals("", exception.getMessage()); // Gets a generated message my the base class
+
+        exception = new RangeException("test");
+        assertEquals("test", exception.getMessage());
+    }
+
+
+    @DisplayName("Test of the  StyleException (summary)")
+    @Test()
+    void styleExceptionTest()
+    {
+        StyleException exception = new StyleException();
+        assertNotEquals("", exception.getMessage()); // Gets a generated message my the base class
+
+        exception = new StyleException("test");
+        assertEquals("test", exception.getMessage());
+
+        IllegalArgumentException  inner = new IllegalArgumentException ("inner message");
+        exception = new StyleException("test", inner);
+        assertEquals("test", exception.getMessage());
+    }
+
+
+    @DisplayName("Test of the WorksheetException (summary)")
+    @Test()
+    void worksheetExceptionTest()
+    {
+        WorksheetException exception = new WorksheetException();
+        assertNotEquals("", exception.getMessage()); // Gets a generated message my the base class
+
+        exception = new WorksheetException("test");
+        assertEquals("test", exception.getMessage());
+    }
+
+
+}
